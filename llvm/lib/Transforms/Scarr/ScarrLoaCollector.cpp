8472f8308855a4242747051dbc774306b9f169af
Jon Kartago Lamida <me@lamida.net>
2021-07-05 00:07:50 +0800

    Tidy up pass output

--- llvm/lib/Transforms/Scarr/ScarrLoaCollector.cpp
+++ llvm/lib/Transforms/Scarr/ScarrLoaCollector.cpp
@@ -60,43 +60,63 @@ void handle(BasicBlock *firstCp,
 
 void collectListOfActions(DominatorTree &DT, Function &function) {
   // We will put BasicBlock in vector for easy reference
-  std::vector<BasicBlock *> basicBlocks;
   std::vector<BasicBlock *> basicBlockCheckpoints;
   if (function.getName() == "main") {
     for (auto it : depth_first(&function.getEntryBlock())) {
-      basicBlocks.push_back(it);
       if (it->getCheckpoint() != Checkpoint::NA) {
         basicBlockCheckpoints.push_back(it);
       }
     }
-  }
 
-  // Pair of Checkpoint_A and Cheeckpont_B
-  std::array<BasicBlock *, 2> checkpointPair = {nullptr, nullptr};
-  // List of Action
-  std::vector<BasicBlock *> LoA;
-  // We will store the measurement here
-  MeasurementMap measurements;
-  for (auto cp : basicBlockCheckpoints) {
-    handle(cp, cp, measurements, LoA, checkpointPair);
-  }
-
-  // Printing the result
-  std::cout << "=============================" << measurements.size() << std::endl;
-  std::cout << "The size of the measurement: " << measurements.size() << std::endl;
-  for (auto iter: measurements) {
-    auto key = iter.first;
-    auto value = iter.second;
-    outs() << "Checkpoint_A: " << *key.first << "\n";
-    outs() << "Checkpoint_B: " << *key.second << "\n";
-
-    int idx = 0;
-    for (auto loa : value) {
-      outs() << "LOA_" << idx << ": " << *loa << "\n";
-      idx++;
+    // Pair of Checkpoint_A and Checkpoint_B
+    std::array<BasicBlock *, 2> checkpointPair = {nullptr, nullptr};
+    // List of Action
+    std::vector<BasicBlock *> LoA;
+    // We will store the measurement here
+    MeasurementMap measurements;
+    for (auto cp : basicBlockCheckpoints) {
+      handle(cp, cp, measurements, LoA, checkpointPair);
     }
-    outs() << "=====================================\n";
+
+    auto loaCount = 0;
+    for (auto measurement : measurements) {
+      auto listOfActions = measurement.second;
+      loaCount+= listOfActions.size();
+    }
+
+    // Printing the result
+    std::cout << "=============================================================" << std::endl;
+    std::cout << "ScaRR Offline Measurement Statistics" << std::endl;
+    std::cout << "=============================================================" << std::endl;
+    std::cout << "Offline Measurement Size: " << measurements.size() << std::endl;
+    std::cout << "Number of Checkpoints: " << basicBlockCheckpoints.size() << std::endl;
+    std::cout << "Number of List of Actions: " << loaCount << std::endl;
+    std::cout << "=============================================================" << std::endl;
+    std::cout << "Checkpoints and LoA Details: " << std::endl;
+    auto mIndex = 0;
+    for (auto measurement : measurements) {
+      auto cpPair = measurement.first;
+      auto listOfActions = measurement.second;
+      std::cout << "=============================================================" << std::endl;
+      std::cout << "Checkpoint " << mIndex << std::endl;
+      std::cout << "LoA Size: " << listOfActions.size() << std::endl;
+      outs() << "Checkpoint_A: " << *cpPair.first << "\n";
+      outs() << "Checkpoint_B: " << *cpPair.second << "\n";
+
+      if (!listOfActions.empty()) {
+        std::cout << "LoA Details: " << std::endl;
+      }
+      int idx = 0;
+      for (auto loa : listOfActions) {
+        outs() << "LOA_" << idx << ": " << *loa << "\n";
+        idx++;
+      }
+      std::cout << "=============================================================" << std::endl;
+      mIndex++;
+    }
   }
+
+
 }
 
 PreservedAnalyses ScarrLoaCollectorPass::run(Function &F, FunctionAnalysisManager &AM) {
