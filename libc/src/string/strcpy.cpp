109e0dbce4c171bfb0c46e49b085ecef58d4781a
Alfonso Gregory <gfunni234@gmail.com>
2021-07-23 16:29:15 -0400

    Update strcpy.cpp

--- libc/src/string/strcpy.cpp
+++ libc/src/string/strcpy.cpp
@@ -63,7 +63,7 @@ static void strcpy_impl(char *__restrict dst, const char *__restrict src,
 LLVM_LIBC_FUNCTION(char *, strcpy,
                    (char *__restrict dest, const char *__restrict src)) {
   strcpy_impl(dest, src, internal::string_length(src) + 1));
-  return dst;
+  return dest;
 }
 
 } // namespace __llvm_libc
