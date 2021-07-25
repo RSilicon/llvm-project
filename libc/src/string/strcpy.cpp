9da400002c8b74b60ae0f6012b67f07273bb79a7
Radical Dweamer <83354299+RadiD234@users.noreply.github.com>
2021-07-25 14:45:49 -0400

    Fix

--- libc/src/string/strcpy.cpp
+++ libc/src/string/strcpy.cpp
@@ -7,9 +7,9 @@
 //===----------------------------------------------------------------------===//
 
 #include "src/string/strcpy.h"
-#include "src/string/string_utils.h"
 #include "src/__support/common.h"
 #include "src/string/memory_utils/elements.h"
+#include "src/string/string_utils.h"
 
 namespace __llvm_libc {
 
